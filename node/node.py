1c309a2b884c683dc5b00ee9a15180b07a5c96f0
Kim Bratzel <kim.bratzel@gmail.com>
2016-08-11 15:28:06 +1000

    Added skeleton node.

---
+++ node/node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+'''CloudWorker Node'''
+
+import multiprocessing
+import os
+
+def run(command):
+    '''Run a command '''
+    os.system('%s' % command)
+
+if __name__ == '__main__':
+
+    #Check secret key with api
+    #Update node info
+
+    #Loop forever
+      #Update time on server
+      #Get config
+
+      #Get actions
+      #Respond to actions
+
+      #Send output to server
+    print 'Starting Cloud Worker Node'
+
+
+
+
+
