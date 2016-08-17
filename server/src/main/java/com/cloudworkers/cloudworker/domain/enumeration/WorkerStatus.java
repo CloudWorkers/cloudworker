b23b5afbc6e178779aeb57ccf323fc629d67c005
Kim Bratzel <kim.bratzel@gmail.com>
2016-08-17 21:48:33 +1000

    Updated UI

--- server/src/main/java/com/cloudworkers/cloudworker/domain/enumeration/WorkerStatus.java
+++ server/src/main/java/com/cloudworkers/cloudworker/domain/enumeration/WorkerStatus.java
@@ -4,5 +4,5 @@ package com.cloudworkers.cloudworker.domain.enumeration;
  * The WorkerStatus enumeration.
  */
 public enum WorkerStatus {
-    PENDING,RUNNING,STOPPED,ERROR
+    ENABLED,DISABLED
 }
